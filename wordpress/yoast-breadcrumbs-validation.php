5cfcf1275a5a629c126fbac336c7d65db9cdeb57
Josh Lake <joshua.charles.lake@gmail.com>
2015-10-08 14:22:15 +0100

    Yoast Breadcrumb Validation

---
+++ wordpress/yoast-breadcrumbs-validation.php
@@ -0,0 +1,6 @@
+add_filter ('wpseo_breadcrumb_output','mc_microdata_breadcrumb');
+function mc_microdata_breadcrumb ($link_output) {
+$link_output = preg_replace(array('#<span xmlns:v="http://rdf.data-vocabulary.org/\#">#','#<span typeof="v:Breadcrumb"><a href="(.*?)" .*?'.'>(.*?)</a></span>#','#<span typeof="v:Breadcrumb">(.*?)</span>#','# property=".*?"#','#</span>$#'), array('','<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="$1" itemprop="url"><span itemprop="title">$2</span></a></span>','<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">$1</span></span>','',''), $link_output);
+
+return $link_output;
+}
